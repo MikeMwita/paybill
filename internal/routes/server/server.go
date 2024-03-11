62fa3432ff86e5fc098513226b1f5d8fdfecc6aa
MikeMwita <mwitamike8@gmail.com>
2024-03-11 13:09:04 +0300

    Define server to initialize handlers

--- internal/routes/server/server.go
+++ internal/routes/server/server.go
@@ -1,1 +1,27 @@
 package server
+
+import (
+	"github.com/MikeMwita/paybill/adapters"
+)
+
+const BaseUrl = "/api"
+
+type Config struct {
+	AuthUsecase adapters.AuthAdapter
+}
+
+//func NewServer(authUseCase adapters.AuthAdapter, cfg config.Config) *gin.Engine {
+//	r := gin.Default()
+//
+//	// Initialize your handlers with necessary dependencies
+//	myHandlers := handlers.NewMyHandlers(authUseCase)
+//
+//	options := handlers.GinServerOptions{
+//		BaseURL: BaseUrl,
+//	}
+//
+//	// Register handlers with options
+//	handlers.RegisterHandlersWithOptions(r, myHandlers, options)
+//
+//	return r
+//}
